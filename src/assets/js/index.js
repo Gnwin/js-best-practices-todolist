8880171d3d3a9bc04d3dc7966ee8810445e837a0
Godwin Nwachukwu <nwgwin@gmail.com>
2022-04-04 22:36:08 +0100

    copy todolist repo

---
+++ src/assets/js/index.js
@@ -0,0 +1,24 @@
+import '../stylesheets/reset.css';
+import '../stylesheets/style.css';
+
+import TodoItems from './modules/TodoItems';
+import grabinput from './modules/GrabUserInput';
+import displayTodos from './modules/DisplayTodos';
+
+let todos;
+if (localStorage.length !== 0) {
+  const b = JSON.parse(localStorage.getItem('todos'));
+  todos = new TodoItems(b);
+} else {
+  todos = new TodoItems([]);
+  localStorage.setItem('todos', JSON.stringify(todos.todoitems));
+}
+
+const addtodoarrow = document.querySelector('.addtodoarrow');
+addtodoarrow.addEventListener('click', grabinput.grab);
+
+const deleteAll = document.querySelector('.clear-link');
+
+deleteAll.onclick = grabinput.clearAllCompleted;
+
+displayTodos.display();
